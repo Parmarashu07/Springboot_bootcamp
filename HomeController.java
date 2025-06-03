eb39b8b4d6848f240c7dd727cea53ec7e942a25f
Ashutosh Kumar <ashutoshkr5115@gmail.com>
2025-06-03 13:37:20 +0530

    initial commit

---
+++ HomeController.java
@@ -0,0 +1,15 @@
+package com.ashusingh.springboot_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world !";
+    }
+}
